366e553e56cdb0f7b7da6bab0d3f768d13cdb812
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2026-09-07 17:41:13 +0530

    feat: add Vercel Analytics for improved tracking

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import NavbarDemo from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <NavbarDemo />
+        <Analytics/>
         {children}
         <Footer />
         </body>
