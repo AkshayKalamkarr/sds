48489cb62c6113d168072ae2a4af36c615d711b3
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2026-09-09 11:06:08 +0530

    fix: correct syntax error in import statement for IndiaMap in page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import CareerswithSupreme from "@/components/CareerswithSupreme";
 import GetInTouch from "@/components/GetInTouch";
 import HeroSection from "@/components/HeroSection";
 import Portfolio from "@/components/Portfolio";
-import IndiaMap from "@/components/IndiaMap
+import IndiaMap from "@/components/IndiaMap";
 
 
 export default function Home() {
